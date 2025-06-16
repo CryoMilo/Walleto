0082142d31768f4eef6cb51c902814f92a7f02e6
CryoMilo <minkojee@gmail.com>
2025-06-17 03:53:05 +0700

    fully setup backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,3 +2,31 @@ import { neon } from "@neondatabase/serverless";
 import "dotenv/config";
 
 export const sql = neon(process.env.DATABASE_URL);
+
+// table
+// -------
+// id serial primay_key
+// user_id varchar
+// title varchar
+// amount decimal
+// category varchar
+// created_at Date
+
+// INITIALIZE DATABASE
+export async function initializeDB() {
+	try {
+		await sql`CREATE TABLE IF NOT EXISTS transactions(
+			id SERIAL PRIMARY KEY,
+			user_id VARCHAR(255) NOT NULL,
+			title VARCHAR(255) NOT NULL,
+			amount DECIMAL(10,2) NOT NULL,
+			category VARCHAR(255) NOT NULL,
+			created_at DATE NOT NULL DEFAULT CURRENT_DATE
+		)`;
+
+		console.log("database initialized successfully");
+	} catch (error) {
+		console.log("Error initializing Database", error);
+		process.exit(1);
+	}
+}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db.js";
 import { rateLimiter } from "./middleware/rateLimiter.js";
 import transactionRoutes from "./routes/transactionRoutes.js";
+import { initializeDB } from "./config/db.js";
 
 dotenv.config();
 
@@ -13,34 +13,6 @@ app.use(express.json());
 app.use(rateLimiter);
 
 const port = process.env.PORT || 5001;
-
-// table
-// -------
-// id serial primay_key
-// user_id varchar
-// title varchar
-// amount decimal
-// category varchar
-// created_at Date
-
-// INITIALIZE DATABASE
-async function initializeDB() {
-	try {
-		await sql`CREATE TABLE IF NOT EXISTS transactions(
-			id SERIAL PRIMARY KEY,
-			user_id VARCHAR(255) NOT NULL,
-			title VARCHAR(255) NOT NULL,
-			amount DECIMAL(10,2) NOT NULL,
-			category VARCHAR(255) NOT NULL,
-			created_at DATE NOT NULL DEFAULT CURRENT_DATE
-		)`;
-
-		console.log("database initialized successfully");
-	} catch (error) {
-		console.log("Error initializing Database", error);
-		process.exit(1);
-	}
-}
 
 app.use("/api/transactions", transactionRoutes);
 
