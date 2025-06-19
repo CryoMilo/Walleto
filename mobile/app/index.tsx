0037e569c9acc8a9d7386ae29cfdd8949129ef07
CryoMilo <minkojee@gmail.com>
2025-06-20 02:38:07 +0700

    added some images

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,17 +1,31 @@
+import { Image } from "expo-image";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-				backgroundColor: "pink",
-			}}>
-			<Text>How about No</Text>
+		<View style={style.container}>
+			<Text style={style.heading}>How about No</Text>
 			<Link href={"/about"}>About</Link>
+
+			<Image
+				source={require("@/assets/images/myanmar_hero.png")}
+				style={{ width: "100%", height: "100%" }}
+			/>
 		</View>
 	);
 }
+
+const style = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "pink",
+		borderRadius: 20,
+	},
+
+	heading: {
+		fontFamily: "serif",
+	},
+});
